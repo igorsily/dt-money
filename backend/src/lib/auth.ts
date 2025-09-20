24e4cffb6a9d1b67e194ffb1cd20a3bdcbbfb475
Igor Sily <igorsily2@gmail.com>
2025-09-20 20:50:06 -0300

    feat: configura better-auth no projeto

---
+++ backend/src/lib/auth.ts
@@ -0,0 +1,33 @@
+import { db } from "@/database/connection";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { openAPI } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  basePath: "/auth",
+  plugins: [openAPI()],
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    usePlural: true,
+  }),
+  advanced: {
+    database: {
+      generateId: false,
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+    password: {
+      hash: (password: string) => Bun.password.hash(password),
+      verify: ({ password, hash }) => Bun.password.verify(password, hash),
+    },
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 5, // 5 minutes
+    },
+  },
+});
